61a5b6ad56f7d9dd64bf98cb3461e0c5f1da5067
Giovani Candido <giovcandido@outlook.com>
2020-09-09 12:38:44 -0300

    Dashboard style update

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import {FiChevronRight} from 'react-icons/fi';
 
-import {Title} from './styles';
+import logo from '../../assets/logo.svg';
+
+import {Title, Form, Repositories} from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositories on GitHub</Title>
+  return (
+    <>
+      <img src={logo} alt="GitHub Explorer" />
+      <Title>Explore repositories on GitHub</Title>
+
+      <Form>
+        <input placeholder="Type in the repository name here" />
+        <button type="submit">Search</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="https://avatars3.githubusercontent.com/u/22887344?s=460&u=fb61edb1343dad0e3b10c4b62c097136bc1e060e&v=4" alt="Giovani Eu" />
+          <div>
+            <strong>github-explorer</strong>
+            <p>GitHub repository explorer made with ReactJS using the GitHub API</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img src="https://avatars3.githubusercontent.com/u/22887344?s=460&u=fb61edb1343dad0e3b10c4b62c097136bc1e060e&v=4" alt="Giovani Eu" />
+          <div>
+            <strong>github-explorer</strong>
+            <p>GitHub repository explorer made with ReactJS using the GitHub API</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img src="https://avatars3.githubusercontent.com/u/22887344?s=460&u=fb61edb1343dad0e3b10c4b62c097136bc1e060e&v=4" alt="Giovani Eu" />
+          <div>
+            <strong>github-explorer</strong>
+            <p>GitHub repository explorer made with ReactJS using the GitHub API</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  )
 }
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,95 @@
 import styled from 'styled-components';
+import {shade} from 'polished';
 
 export const Title = styled.h1`
   font-size: 48px;
   color: #3a3a3a;
+  max-width: 450px;
+  line-height: 56px;
+  margin-top: 80px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+  display: flex;
+
+  input{
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+
+    &::placeholder{
+      color: #a8a8b3;
+    }
+  }
+
+  button{
+    width: 210px;
+    height: 70px;
+    background-color: #04D361;
+    border: 0;
+    border-radius: 0 5px 5px 0;
+    color: #fff;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    &:hover{
+      background-color: ${shade(0.2, '#04D361')}; 
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a{
+    background-color: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    & + a{
+      margin-top: 16px;
+    }
+    
+    &:hover{
+      transform: translateX(20px);
+    }
+
+    img{
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div{
+      margin-left: 16px;
+
+      strong{
+        font-size: 20px;
+        color: #3d4d4d;
+      }
+
+      p{
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg{
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
 `;
