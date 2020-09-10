d54d49019f08ac7a7a1c34b53a095f62220d6bd4
Giovani Candido <giovcandido@outlook.com>
2020-09-10 19:05:06 -0300

    Repository styled page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import {useRouteMatch} from 'react-router-dom';
+import {useRouteMatch, Link} from 'react-router-dom';
+import {FiChevronLeft, FiChevronRight} from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
+
+import {Header, RepositoryInfo, Issues} from './style';
 
 interface RepositoryParams{
   repository: string;
@@ -8,7 +13,50 @@ interface RepositoryParams{
 const Repository: React.FC = () => {
   const {params} = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="GitHub Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="null" alt="owner avatar"  />
+          <div>
+            <strong>Repository: {params.repository}</strong>
+            <p>Repo description</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>StarsNum</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>ForksNum</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>OpenIssuesNum</strong>
+            <span>Open Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="nowhere">
+          <div>
+            <strong>Issue</strong>
+            <p>Issue issue</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  )
 }
 
 export default Repository;
