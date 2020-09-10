ac87f19b54451f4d3f85248a80ddd1a6df947b99
Giovani Candido <giovcandido@outlook.com>
2020-09-10 18:30:25 -0300

    Fixing routes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect, FormEvent} from 'react';
+import {Link} from 'react-router-dom';
 import {FiChevronRight} from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -70,14 +71,14 @@ const Dashboard: React.FC = () => {
       
       <Repositories>
         {repositories.map(repository => (
-          <a key={repository.full_name} href="teste">
+          <Link key={repository.full_name} to={`/repository/${repository.full_name}`}>
             <img src={repository.owner.avatar_url} alt={repository.owner.login} />
             <div>
               <strong>{repository.full_name}</strong>
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
