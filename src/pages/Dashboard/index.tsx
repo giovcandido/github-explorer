e11a748d89ec509da653ea183de69755723d2196
Giovani Candido <giovcandido@outlook.com>
2020-09-10 18:10:40 -0300

    Using local storage to save repositories

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState, FormEvent} from 'react';
+import React, {useState, useEffect, FormEvent} from 'react';
 import {FiChevronRight} from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -18,7 +18,19 @@ interface Repository{
 const Dashboard: React.FC = () => {
   const [newRepoSearch, setNewRepoSearch] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem('@github-explorer:repositories');
+
+    if(storagedRepositories){
+      return JSON.parse(storagedRepositories);
+    }
+
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('@github-explorer:repositories', JSON.stringify(repositories));
+  }, [repositories]);
 
   async function handleNewRepoSearch(event: FormEvent<HTMLFormElement>): Promise<void>{
     event.preventDefault();
