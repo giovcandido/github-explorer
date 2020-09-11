49f9510784b220159b67ad8e4b56639762e1fc33
Giovani Candido <giovcandido@outlook.com>
2020-09-11 12:58:51 -0300

    Reverting global Repository component

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -5,6 +5,78 @@ interface FormAdditionalProps{
   hasError: boolean;
 };
 
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+`;
+
+export const RepositoryInfo = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  button{
+    height: 30px;
+    width: 30px;
+    margin-right: 10px;
+    border: #fff solid 1px;
+    border-radius: 50%;
+    background-color: #fff;
+    transition: transform 0.2s;
+
+    &:hover{
+      transform: translateY(-5px);
+    }
+  }
+
+  & + section{
+    margin-top: 16px;
+  }
+
+  a{
+    background-color: #fff;
+    border-radius: 5px;
+    padding: 24px;
+    display: block;
+    display: flex;
+    flex: 1;
+    text-decoration: none;
+    align-items: center;
+    transition: transform 0.2s;
+  
+    &:hover{
+      transform: translateX(20px);
+    }
+
+    img{
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div{
+      margin: 0 16px;
+      flex: 1;
+
+      strong{
+        font-size: 20px;
+        color: #3d4d4d;
+      }
+
+      p{
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg{
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
+
 export const Form = styled.form<FormAdditionalProps>`
   margin-top: 40px;
   max-width: 700px;
