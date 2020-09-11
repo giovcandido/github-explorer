4654d8a9b91285e81177b05d55d155a3c7384186
Giovani Candido <giovcandido@outlook.com>
2020-09-11 11:18:04 -0300

    Updating global style with some shared components

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import {createGlobalStyle} from 'styled-components';
+import styled, {createGlobalStyle} from 'styled-components';
 
 import background from '../assets/background.svg';
 
@@ -28,4 +28,34 @@ export default createGlobalStyle`
     margin: 0 auto;
     padding: 40px 20px;
   }
+`;
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  a{
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: #a8a8b3;
+    transition: color 0.2s;
+
+    &:hover{
+      color: #666;
+    }
+
+    svg{
+      margin-right: 4px;
+    }
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+  max-width: 450px;
+  line-height: 56px;
+  margin-top: 80px;
 `;
