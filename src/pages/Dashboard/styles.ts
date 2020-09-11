ae0e54f8b20fb22347ec8b5adb95d097dd620d19
Giovani Candido <giovcandido@outlook.com>
2020-09-11 11:18:54 -0300

    Removing shared components from page style

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -5,14 +5,6 @@ interface FormAdditionalProps{
   hasError: boolean;
 };
 
-export const Title = styled.h1`
-  font-size: 48px;
-  color: #3a3a3a;
-  max-width: 450px;
-  line-height: 56px;
-  margin-top: 80px;
-`;
-
 export const Form = styled.form<FormAdditionalProps>`
   margin-top: 40px;
   max-width: 700px;
--- src/pages/Repository/style.ts
+++ src/pages/Repository/style.ts
@@ -1,27 +1,5 @@
 import styled from 'styled-components';
 
-export const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  a{
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    color: #a8a8b3;
-    transition: color 0.2s;
-
-    &:hover{
-      color: #666;
-    }
-
-    svg{
-      margin-right: 4px;
-    }
-  }
-`;
-
 export const RepositoryInfo = styled.section`
   margin-top: 80px;
 
