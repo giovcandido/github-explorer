e3f92d725049ffaa4ea541f6e43f4465f28fcbca
Giovani Candido <giovcandido@outlook.com>
2020-09-11 11:38:30 -0300

    Saved repos page structure and listing

--- src/pages/SavedRepositories/index.tsx
+++ src/pages/SavedRepositories/index.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
-import {FiChevronLeft} from 'react-icons/fi';
+import {FiChevronLeft, FiChevronRight} from 'react-icons/fi';
 
 import logo from '../../assets/logo.svg';
 
-import {Header, Title} from '../../styles/global';
+import {Header, Title, Repositories} from '../../styles/global';
+
+interface Repository{
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
 
 const SavedRepositories: React.FC = () => {
+  const [savedRepos] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem('@github-explorer:repositories');
+      if(storagedRepositories){
+        return JSON.parse(storagedRepositories);
+      }
+
+      return [];
+  });
+
   return (
     <>
       <Header>
@@ -18,6 +36,19 @@ const SavedRepositories: React.FC = () => {
       </Header>
 
       <Title>Here are your saved repositories</Title>
+      
+      <Repositories>
+        {savedRepos.map(repository => (
+          <Link key={repository.full_name} to={`/repository/${repository.full_name}`}>
+            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
+      </Repositories>
     </>
   )
 }
